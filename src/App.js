5872b720fcf7a74bea8e3742bc143023577aef75
Adrian Sian <adriansian@gmail.com>
2021-08-08 22:20:37 +0800

    covid19: initialize structure
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 
 function App() {
   return ( //BEM NAMING CONVENTION
-    <div className="app"> 
+    <div className="app">
       <h1>Let's build a COVID19 tracker app</h1>
 
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+       {/* Infobox */}
+       {/* Infobox */}
+       {/* Infobox */}
+
+       {/* Table */}
+       {/* Graph */}
+
+       {/* Map */}
     </div>
   );
 }
