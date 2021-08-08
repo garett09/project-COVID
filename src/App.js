f61cd2d1df8e5340cdde9ed5956b2567f79d2830
Adrian Sian <adriansian@gmail.com>
2021-08-09 00:11:04 +0800

    covid19: implement useEffect that is being used to call the API
    API use and useEffect
    
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormControl } from '@material-ui/core';
 import {
   MenuItem,
@@ -7,36 +7,39 @@ import {
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState(['USA', 'UK', 'JAPAN']);
-
+  const [countries, setCountries] = useState([]);
   //API https://disease.sh/v3/covid-19/countries
   //STATE = How to write a variable in react
-
   //useEEFFECT: run a piece of code based on a given condition
-
   useEffect(() => {
     //The code inside here will run once when the component loads and not again after
     //async: send a request, wait for it, do something with it
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso3
+            }
+          ));
+          setCountries(countries);
+        });
+    }
+    getCountriesData();
   }, []);
-
-
   return ( //BEM NAMING CONVENTION
     <div className="app">
       <div className="app__header">
         <h1>Coronavirus Tracker</h1>
         <FormControl className="app_dropdown">
           <Select variant="outlined" value="ABC">
-
             {
               countries.map(country => (
-                <MenuItem value={country}>{country}</MenuItem>
+                <MenuItem value={country.value}>{country.name}</MenuItem>
               ))
             }
-
-            {/*<MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">2</MenuItem>
-          <MenuItem value="worldwide">3</MenuItem>
-          <MenuItem value="worldwide">Wideeee</MenuItem> */}
           </Select>
         </FormControl>
       </div>
