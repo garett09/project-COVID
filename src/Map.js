7c600636db9cdd9760af11bb571ac3f1d59edfdd
Adrian Sian <adriansian@gmail.com>
2021-08-11 23:26:38 +0800

    covid19: initiate Map,js
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import"./Map.css"
+import { MapContainer as LeafletMap, TileLayer} from 'react-leaflet';
 
 function Map() {
     return (
-        <div>
-          <h1>I am a map</h1>  
-        </div>
+        <div className="map_box">
+     <LeafletMap className="map">
+       <TileLayer
+         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+       />
+     </LeafletMap>
+   </div>
     )
 }
 
